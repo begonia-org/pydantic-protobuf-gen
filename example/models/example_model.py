3fa56797109e8fdd22fb01a25c2fc49e4d42d8b1
qinzhonghe <qinzhonghe@51qqt.com>
2024-06-30 18:43:10 +0800

    fix:fix required annot not woring

--- example/models/example_model.py
+++ example/models/example_model.py
@@ -8,7 +8,6 @@
 '''
 
 
-
 from pydantic import BaseModel, Field
 
 from typing import List, Optional, Dict, Any
@@ -18,7 +17,6 @@ from enum import Enum
 import datetime
 
 
-
 class ExampleType(Enum):
     UNKNOWN = 0
     TYPE1 = 1
@@ -26,14 +24,15 @@ class ExampleType(Enum):
     TYPE3 = 3
 
 
-
-
 class Example(BaseModel):
-    name: Optional[str] = Field(description="Name of the example",example="'ohn Doe",default="John Doe",alias="full_name")
-    age: Optional[int] = Field(description="Age of the example",example=30,default=30,alias="years")
+    name: Optional[str] = Field(description="Name of the example",
+                                example="'ohn Doe", default="John Doe", alias="full_name")
+    age: Optional[int] = Field(
+        description="Age of the example", example=30, default=30, alias="years")
     emails: Optional[List[str]] = Field(description="Emails of the example")
-    entry: Optional[Dict[str,Any]] = Field(description="Properties of the example")
-    created_at: datetime.datetime = Field(description="Creation date of the example",default=datetime.datetime.now(),schema_extra={'required': True})
-    type: Optional[ExampleType] = Field(description="Type of the example",default=ExampleType.TYPE1)
-
-
+    entry: Optional[Dict[str, Any]] = Field(
+        description="Properties of the example")
+    created_at: datetime.datetime = Field(
+        description="Creation date of the example", default=datetime.datetime.now(), schema_extra={'required': True})
+    type: Optional[ExampleType] = Field(
+        description="Type of the example", default=ExampleType.TYPE1)
