ae5950e4eba5b7407e245d56db2474e8c0827e12
Your Name <you@example.com>
2024-11-21 16:50:14 +0800

    feat:add protobuf_pydantic_out

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-python3 -m grpc_tools.protoc --proto_path=.  --proto_path=./ --python_out=./protobuf_pydantic_gen --pyi_out=./protobuf_pydantic_gen --grpc_python_out=./protobuf_pydantic_gen ./protobuf_pydantic_gen/*.proto
+python3 -m grpc_tools.protoc --proto_path=.  --proto_path=./ --python_out=./ --pyi_out=./ --grpc_python_out=./ ./protobuf_pydantic_gen/*.proto
